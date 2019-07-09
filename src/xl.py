d96d7d9abafb6bd188261c07046d5845ee196fcb
Vadim Shkaberda <V.Shkaberda@gmail.com>
2019-07-09 15:21:28 +0300

    Changed formatting when Export to excel

--- src/xl.py
+++ src/xl.py
@@ -4,10 +4,11 @@ Created on Fri Jul  5 12:24:40 2019
 
 @author: v.shkaberda
 """
+from decimal import Decimal
 from log_error import writelog
 from pathlib import Path
 from openpyxl import Workbook
-from openpyxl.styles import Border, Font, Side
+from openpyxl.styles import Alignment, Border, Font, Side, PatternFill
 from openpyxl.utils import get_column_letter
 from os import path
 import time
@@ -21,13 +22,15 @@ def export_to_excel(headers, rows):
         ws.append(key for key in headers.keys())
         # Rows
         for row in rows:
-            ws.append(row)
+            ws.append(tuple(row))
         # Customizing style
         for col, width in enumerate(headers.values()):
             cell = ws.cell(row=1, column=col+1)
+            cell.alignment = Alignment(horizontal="center")
             cell.font = Font(bold=True)
             cell.border = Border(bottom=Side(border_style='thin',
                                              color='FF000000'))
+            cell.fill = PatternFill("solid", fgColor="D3D3D3")
             ws.column_dimensions[get_column_letter(col+1)].width = width / 5
         # Save the file
         time_txt = time.strftime("%d-%m-%Y %H.%M.%S", time.localtime())
